c64a44430dd191c2172c6a9160f1fc79611d6340
SANTOSHKUMAR2005 <sank224203@gmail.com>
2024-03-03 10:08:47 +0530

    add files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # santosh
 my self santosh kumar
-I'm from uttar Pradesh ayodhaya
+I'm from uttar Pradesh ayodhaya 
---
+++ hackslash.js
@@ -0,0 +1,10 @@
+function hackslash(){
+    let currentmod="white"
+    if (currentmod=="white") {
+       document.querySelector("body").style.backgroundColor="black";
+       document.querySelector("body").style.color="white"; 
+    } else {
+        
+    }
+   
+}
